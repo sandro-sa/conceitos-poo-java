9b1fd00c77601f6a55ab35b5beac8615707c6a9d
sandro-sa <sandrosa0315@outlook.com>
2024-10-01 10:04:32 -0300

    Polimorfismo

--- src/desafio_dio_banco/Conta.java
+++ src/desafio_dio_banco/Conta.java
@@ -2,9 +2,17 @@ package desafio_dio_banco;
 
 public abstract class Conta  implements IConta{
 
-	private int agencia;
-	private int numero;
+	private static final int AGENCIA_PADRAO = 1;
+	private static int SEQUENCIAL = 1;
+	
+	protected int agencia;
+	protected int numero;
 	private double saldo;
+	
+	public Conta() {
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.numero = SEQUENCIAL++;
+	}
 	
 	@Override
 	public void sacar(double valor) {
--- src/desafio_dio_banco/ContaPoupanca.java
+++ src/desafio_dio_banco/ContaPoupanca.java
@@ -2,5 +2,5 @@ package desafio_dio_banco;
 
 public class ContaPoupanca extends Conta  {
 
-	
+
 }
